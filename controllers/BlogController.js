7d88a02f1ec2b05968b3e41bba4199562ad314fa
Guilherme Defavori Fabretti <guidfabretti@hotmail.com>
2024-03-29 15:18:10 -0300

    iniciando back

---
+++ controllers/BlogController.js
@@ -0,0 +1,23 @@
+class BlogController{
+  index(req, res) {
+    res.render("index.ejs");
+  }
+
+  show() {
+
+  }
+
+  store() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+    
+  }
+}
+
+export default new BlogController();
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import BlogController from "./controllers/BlogController.js";
 
 const app = express();
 const port = 3000;
@@ -9,9 +10,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+app.get("/", BlogController.index)
 
 app.listen(port, () => {
   console.log(`🔥 Server started at http://localhost:${port}`);
